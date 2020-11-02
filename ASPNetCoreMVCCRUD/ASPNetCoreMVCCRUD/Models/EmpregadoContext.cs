c525e2c0fefa65728cd34f5995d05b29d05f0f73
Hudson <hudsonrodrigo06@gmail.com>
2020-11-02 09:32:11 -0300

    Criando e adicionando contexto ao Startup>ConfigureServices

---
+++ ASPNetCoreMVCCRUD/ASPNetCoreMVCCRUD/Models/EmpregadoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMVCCRUD.Models
+{
+	public class EmpregadoContext:DbContext
+	{
+		public EmpregadoContext(DbContextOptions<EmpregadoContext> options):base(options)
+		{
+
+		}
+	}
+}
